918a549eae66f342b16efd48dca72333ecd7e888
cutnop <cutnop@muohio.edu>
2014-09-17 12:36:25 -0400

    Removed cout text and adjusted matrix prints to match example output formating

--- project1/project1.cpp
+++ project1/project1.cpp
@@ -6,21 +6,21 @@ int m1row, m1col, m2row, m2col;
 
 int main(){
 	// Row & Col input
-	cout << "Enter the number of ROWs for matrix 1: ";
+	//cout << "Enter the number of ROWs for matrix 1: ";
 	cin >> m1row;
-	cout << "Enter the number of COLs for matrix 1: ";
+	//cout << "Enter the number of COLs for matrix 1: ";
 	cin >> m1col;
-	cout << "Enter the number of ROWs for matrix 2: ";
+	//cout << "Enter the number of ROWs for matrix 2: ";
 	cin >> m2row;
 
 	// Proper sizes check
 	if(m1col != m2row){
-		cout << "Cols of matrix 1 must match Rows of matrix 2. Try again.\n";
+		//cout << "Cols of matrix 1 must match Rows of matrix 2. Try again.\n";
 		return 1;
 	}
 	
 	// Row & Col input cont.
-	cout << "Enter the number of COLs for matrix 2: ";
+	//cout << "Enter the number of COLs for matrix 2: ";
 	cin >> m2col;
 
 	// Init. matrix 1 & 2
@@ -34,7 +34,7 @@ int main(){
 	}
 
 	// Fill matrix 1
-	cout << "Enter values for matrix 1 (one at a time)\n";
+	//cout << "Enter values for matrix 1 (one at a time)\n";
 	for(int y = 0; y < m1row; y++){
 		for(int x = 0; x < m1col; x++){
 			cin >> M1[x][y];
@@ -42,7 +42,7 @@ int main(){
 	}
 	
 	// Fill matrix 2
-	cout << "\nEnter values for matrix 2 (one at a time)\n";
+	//cout << "\nEnter values for matrix 2 (one at a time)\n";
         for(int y = 0; y < m2row; y++){
                 for(int x = 0; x < m2col; x++){
                         cin >> M2[x][y];
@@ -59,12 +59,13 @@ int main(){
 	}
 
 	// Print responses
-	cout << "\n--Output--\n";
+	//cout << "\n--Output--\n";
 
 	// Print matrix 1
 	for(int y = 0; y < m1row; y++){
 		for(int x = 0; x < m1col; x++){
-                        cout <<  M1[x][y] << " ";
+                        cout <<  M1[x][y];
+			if(x < m1col-1) cout << " ";
                 }
 		cout << "\n";
 	}	
@@ -73,7 +74,8 @@ int main(){
 	// Print matrix 2
 	for(int y = 0; y < m2row; y++){
                 for(int x = 0; x < m2col; x++){
-                        cout <<  M2[x][y] << " ";
+                        cout <<  M2[x][y];
+			if(x < m2col-1) cout << " ";
                 }
                 cout << "\n";
         }
@@ -82,7 +84,8 @@ int main(){
 	// Print matrix 3
 	for(int y = 0; y < m1row; y++){
                for(int x = 0; x < m2col; x++){
-                       cout <<  M3[x][y] << " ";
+                       cout <<  M3[x][y];
+			if(x < m2col-1) cout << " ";
                }
                cout << "\n";
         }
