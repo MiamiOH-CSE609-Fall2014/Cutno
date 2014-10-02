7e5e8de41664d7e3f39f7f4eb16ad887ba2475d5
cutnop <cutnop@muohio.edu>
2014-10-02 19:18:03 -0400

    Done, just need to format stuff now

--- project2/project2.cpp
+++ project2/project2.cpp
@@ -8,6 +8,7 @@ using namespace std;
 
 vector<string> names;
 vector<double> grades;
+vector<char> letterGrade;
 int vecIndex = 0;
 
 string tempName  = "";
@@ -58,27 +59,66 @@ void getNamesGrades(){
 
 }
 
+void doMath(){
+	//clculate average grade
+        for(int i = 0; i < grades.size(); i++){
+                average += grades[i];
+        }
+        average = average/grades.size();
+
+        //clculate standard deviation
+        for(int i = 0; i < grades.size(); i++){
+                deviation += pow( (grades[i] - average), 2.0);
+        }
+        deviation = sqrt(deviation/grades.size());
+}
+
+char findLetter(double grade){
+	if(grade > average+(1.5*deviation))
+		return 'A';
+	else if(average+(.5*deviation) < grade && grade < average+(1.5*deviation))
+		return 'B';
+	else if(average+(-.5*deviation) < grade && grade < average+(.5*deviation))
+		return 'C';
+	else if(average+(-1.5*deviation) < grade && grade < average+(-.5*deviation))
+		return 'D';
+	else
+		return 'F';
+}
+
+void printScale(){
+	cout << "\n\n";
+	cout << "Grading scale" << endl;
+	if(average+(1.5*deviation) > 100)
+		cout << "A above 100%" << endl;
+	else
+		cout << "A above " << average+(1.5*deviation) << "%" << endl;
+	cout << "B " << average+(.5*deviation) << "% - " << average+(1.5*deviation) << "%" << endl;
+	cout << "C " << average+(-.5*deviation) << "% - " << average+(.5*deviation) << "%" << endl;
+	cout << "D " << average+(-1.5*deviation) << "% - " << average+(-.5*deviation) << "%" << endl;
+	cout << "F below " << average+(-1.5*deviation) << "%\n" << endl;
+}
+
 int main (){
 	cout << "Welcome to the grade curve system.\nEnter students full name followed by the grade earned.\nEnter lowercase <quit> as a name to stop taking input.\n\n";
 	
 	//get inputs
 	getNamesGrades();
 	
-	//clculate average grade
-	for(int i = 0; i < grades.size(); i++){
-		average += grades[i];
-	}
-	average = average/grades.size();
+	//clculate average and deviation
+	doMath();
 	
-	//clculate standard deviation
+	//print scale
+	printScale();
+
+	//find letter grade after curve
 	for(int i = 0; i < grades.size(); i++){
-		deviation += pow( (grades[i] - average), 2.0);
+		letterGrade.push_back(findLetter(grades[i]));
 	}
-	deviation = sqrt(deviation/grades.size());	
 
-
+	//print
 	for(int i = 0; i < names.size(); i++){
-		cout << names[i] << "\t" << grades[i] << "\n";
+		cout << names[i] << "\t" << grades[i] << "\t" << letterGrade[i] << "\n";
 	}	
 
 	return 0;
