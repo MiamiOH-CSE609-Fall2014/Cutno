fa4855dc2eb521fee93bce8053d6d68d2bb66f06
cutnop <cutnop@muohio.edu>
2014-11-10 00:23:33 -0500

    started 2.1

--- project3/p3.cpp
+++ project3/p3.cpp
@@ -3,8 +3,11 @@
 #include<string>
 #include<vector>
 #include<fstream>
+#include<map>
 
 using namespace std;
+string digrams[] = {"AA", "AG", "AC", "AT", "GA", "GG", "GC", "GT", "CA", "CG", "CC", "CT", "TA", "TG", "TC", "TT"};
+
 
 tuple< string, vector<string>, string> parseFastaFile(string path){
 	string header = "";
@@ -31,9 +34,33 @@ tuple< string, vector<string>, string> parseFastaFile(string path){
 	return make_tuple(header, comments, sequence);
 }
 
+map< string, int > digramFreqScores(string s){
+	map< string, int >scores;
+
+	for(int i = 0; i < 16; i++){
+		string dnaTemp = s;
+		int count = 0;
+
+		int index = 0;
+		while(true){
+			index = dnaTemp.find(digrams[i]);
+			if(index == string::npos){
+				break;
+			}else{
+				dnaTemp.replace(index, 1, "X");
+				count++;
+			}
+		}
+		scores[digrams[i]] = count;
+	}
+	return scores;
+}
+
+
+
 int main(){
 	string file1, file2;
-	
+
 	cout << "Enter .txt file name: ";
 	cin >> file1;
 	tuple<string, vector<string>, string> parsed = parseFastaFile(file1);
@@ -44,5 +71,8 @@ int main(){
 	}
 	cout << endl << get<2>(parsed) << endl << endl;
 */
+
+	map< string, int > scoreMap = digramFreqScores(get<2>(parsed));
+
 	return 0;
 }
